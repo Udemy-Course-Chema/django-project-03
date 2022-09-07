c74d45bf7d01abc86893d954d1d2ad553483d34a
Jose Maria <chchema26@gmail.com>
2022-09-07 06:36:19 -0500

    Added Reset Password Pages

--- root/settings.py
+++ root/settings.py
@@ -137,3 +137,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Auth Redirects
 LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Emails
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR,"sent_emails")
+else:
+    #Email en Producción
+    pass
+
